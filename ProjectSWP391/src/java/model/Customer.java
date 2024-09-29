da700bbfb9c4a83e36b5fb7b364d0674c4c95ab3
tranhuy2ca2 <tranhuy2ca2@gmail.com>
2024-09-30 03:06:46 +0700

    login

---
+++ ProjectSWP391/src/java/model/Customer.java
@@ -0,0 +1,110 @@
+package model;
+
+import java.sql.Date;
+
+public class Customer {
+
+    private int userID;
+    private String userName;
+    private String password;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String role;
+    private String address;
+    private Date createdAt;
+
+    public Customer() {
+    }
+
+    public Customer(int userID, String userName, String password, String fullName, String email, String phone, String role, String address, Date createdAt) {
+        this.userID = userID;
+        this.userName = userName;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.role = role;
+        this.address = address;
+        this.createdAt = createdAt;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" + "userID=" + userID + ", userName=" + userName + ", password=" + password + ", fullName=" + fullName + ", "
+                + "email=" + email + ", phone=" + phone + ", role=" + role + ", address=" + address + ", createdAt=" + createdAt + '}';
+    }
+
+}
--- ProjectSWP391/web/Header.jsp
+++ ProjectSWP391/web/Header.jsp
@@ -5,6 +5,7 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -46,13 +47,19 @@
             <div class="container">
                 <div class="menu-bg-wrap">
                     <div class="site-navigation">
-                        <a href="index.html" class="logo m-0 float-start">Property</a>
+                        <a href="homepage" class="logo m-0 float-start">Property</a>
 
                         <ul
                             class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end"
                             >
-                            <li class="active"><a href="index.html">Home</a></li>
-                            <li><a href="#">Login</a></li>
+                            <li class="active"><a href="homepage">Home</a></li>
+                            <c:if test="${sessionScope.user==null}">
+                                <li><a href="sign_in.jsp">Login</a></li>
+                            </c:if>
+                            <c:if test="${sessionScope.user!=null}">
+                                <li><a href="logout">Log out</a></li>
+                                <li class="active"><a href="#">Hello ${user.fullName}</a></li>
+                            </c:if>    
                             <li class="has-children">
                                 <a href="properties.html">Properties</a>
                                 <ul class="dropdown">
